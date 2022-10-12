b50e70118401dcb823e756459348d9c918cac0e1
Jay Tyagi <94462361+JayTyagi01@users.noreply.github.com>
2022-10-12 21:40:28 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+    name: "Kya Mujhe Pyar He",
+    artist: "K.K",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Dil Ibaadat",
+    artist: "K.k",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Aankho Mein Teri",
+    artist: "K.k",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Tu Jo Mila",
+    artist: "K.K",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Zara Sa",
+    artist: "K.K",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Khuda Jaane",
+    artist: "K.K",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Zara Zara",
+    artist: "Jal Raj",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Kabhi Jo Baadal",
+    artist: "Arijit Singh",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Hasi Bangye",
+    artist: "Ami Mishra",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Tum Hi Aana",
+    artist: "Jubin Nautiyal",
+    img: "music-10",
+    src: "music-10"
+  },
+];
